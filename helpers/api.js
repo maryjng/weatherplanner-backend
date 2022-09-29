99376d04e0a5406983638ba3fcebf72b995f47f7
Mary <68235230+maryjng@users.noreply.github.com>
2022-09-29 11:21:04 -0400

    Add files via upload

--- helpers/api.js
+++ helpers/api.js
@@ -1,13 +1,20 @@
 const db = require("../db");
 
 // takes zipcode and returns latitude and longitude for weather api request use
-async function getLatAndLong(data) {
-    let { zipcode } = data
+async function getLatAndLong(zipcode) {
+    let leading_zeroes = 0
+    
+    if (zipcode[0] == 0 && zipcode[1] == 0) {
+        leading_zeroes = 2
+    } else if (zipcode[0] == 0) {
+        leading_zeroes = 1
+    }
+
     let res = await db.query(
         `SELECT latitude, longitude
         FROM zipcodes
-        WHERE zipcode=$1
-        `, [zipcode]
+        WHERE zipcode=$1 and leading_zeroes=$2
+        `, [zipcode, leading_zeroes]
     );
     let result = res.rows[0]
     if (!result) return false;
