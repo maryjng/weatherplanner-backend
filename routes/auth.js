3342031a993e7bf344f47f87b482c96d43f15f6c
Mary <68235230+maryjng@users.noreply.github.com>
2022-11-07 23:22:18 -0500

    Add files via upload

--- routes/auth.js
+++ routes/auth.js
@@ -8,8 +8,8 @@ const User = require("../models/user");
 const express = require("express");
 const router = new express.Router();
 const { createToken } = require("../helpers/tokens");
-const userAuthSchema = require("../schemas/userAuth.json");
-const userRegisterSchema = require("../schemas/userRegister.json");
+const userAuthSchema = require("../schemas/userAuth");
+const userRegisterSchema = require("../schemas/newUser");
 const { BadRequestError } = require("../expressError");
 
 /** POST /auth/token:  { username, password } => { token }
